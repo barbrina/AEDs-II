9625efd966c38a391563a618909652c4baf08874
Bárbara Gualberto <62162582+barbrina@users.noreply.github.com>
2022-11-25 02:15:49 -0300

    Add files via upload

--- Analisando conexões do Twitter/src/twitter.py
+++ Analisando conexões do Twitter/src/twitter.py
@@ -7,8 +7,8 @@ import tweepy
 import json
 import pandas as pd
 
-# Aqui utilizamos a funÃ§Ã£o open para abrir nosso arquivo e a
-# biblioteca json para carregar nosso arquivo para uma variÃ¡vel chamada info.
+# Aqui utilizamos a funcaoo open para abrir nosso arquivo e a
+# biblioteca json para carregar nosso arquivo para uma variavel chamada info.
 credenciais = open('credenciais.json').read()
 info = json.loads(credenciais)
 
@@ -17,7 +17,7 @@ consumer_secret = info['API_ACCESS_SECRET_BARBARA']
 access_key = info['ACCESS_TOKEN_BARBARA']
 access_secret = info['ACCESS_TOKEN_SECRET_BARBARA']
 
-# Setup tweepy to authenticate with Twitter credentials:
+# Configure tweepy para autenticar com as credenciais do Twitter:
 autorizacao = tweepy.OAuthHandler(consumer_key, consumer_secret)
 autorizacao.set_access_token(access_key, access_secret)
 
@@ -41,15 +41,15 @@ for user in user_list:
         continue
     follower_list.append(followers)
 
-df = pd.DataFrame(columns=['source', 'target'])  # Empty DataFrame
-# Set the list of followers as the target column
+df = pd.DataFrame(columns=['source', 'target'])  # DataFrame vazio
+# Defina a lista de seguidores como a coluna de destino
 df['target'] = follower_list[0]
-df['source'] = me.id  # Set my user ID as the source
+df['source'] = me.id  # Define meu ID de usuário como source
 
 display(df)
 
-G = nx.from_pandas_edgelist(df, 'source', 'target')  # Turn df into graph
-pos = nx.spring_layout(G)  # specify layout for visual
+G = nx.from_pandas_edgelist(df, 'source', 'target')  # Transforma df em gráfico
+pos = nx.spring_layout(G)  # especifica layout 
 
 f, ax = plt.subplots(figsize=(10, 10))
 plt.style.use('ggplot')
@@ -62,24 +62,24 @@ nx.draw(G)
 plt.savefig("BarbrinassFollowers.png")
 
 
-# Use the list of followers we extracted in the code above i.e. my 62 followers
+# Use a lista de seguidores que extraímos no código acima
 user_list = list(df['target'])
 for userID in user_list:
     print(userID)
     followers = []
     follower_list = []
 
-    # fetching the user
+    # busca o usuário
     user = api.get_user(user_id=userID)
 
-    # fetching the followers_count
+    # buscan a contagem de seguidores
     followers_count = user.followers_count
 
     try:
         for page in tweepy.Cursor(api.get_follower_ids, user_id=userID).pages():
             followers.extend(page)
             print(len(followers))
-            if followers_count >= 5000:  # Only take first 5000 followers
+            if followers_count >= 5000:  # Pega apenas os primeiros 5.000 seguidores
                 break
     except tweepy.errors.TweepyException:
         print("error")
@@ -92,13 +92,12 @@ for userID in user_list:
     df.to_csv("networkOfFollowers.csv")
 
 
-df = pd.read_csv("networkOfFollowers.csv")  # Read into a df
-
+df = pd.read_csv("networkOfFollowers.csv")  # Lê em um df
 display(df)
 
 G = nx.from_pandas_edgelist(df, 'source', 'target')
 
-G.number_of_nodes()  # Find the total number of nodes in this graph
+G.number_of_nodes()  # Encontra o número total de nós neste gráfico
 
 G_sorted = pd.DataFrame(sorted(G.degree, key=lambda x: x[1], reverse=True))
 G_sorted.columns = ['nconst', 'degree']
@@ -107,10 +106,10 @@ G_sorted.head()
 #u = api.get_user(37728789)
 # u.screen_name
 
-G_tmp = nx.k_core(G, 4)  # Exclude nodes with degree less than 10
+G_tmp = nx.k_core(G, 4)  # Exclui nós com grau menor que 4
 
 partition = community_louvain.best_partition(
-    G_tmp)  # Turn partition into dataframe
+    G_tmp)  # Transforma partição em dataframe
 partition1 = pd.DataFrame([partition]).T
 partition1 = partition1.reset_index()
 partition1.columns = ['names', 'group']
@@ -141,7 +140,6 @@ nx.draw_networkx_labels(G_tmp, pos, font_size=4)
 nx.draw_networkx_edges(G_tmp, pos, width=1.0, alpha=0.2)
 plt.savefig('twitterFollowers.png')
 
-# I've found Gephi really likes when your node column is called 'Id'
 combined = combined.rename(columns={"names": "Id"})
 edges = nx.to_pandas_edgelist(G_tmp)
 nodes = combined['Id']
